d7c3942871911477aedcce612169c6ceb520fa03
Alvonso N <102427335+V0NSO@users.noreply.github.com>
2023-12-03 21:23:32 +0700

    Update home.js

--- src/scripts/views/pages/home.js
+++ src/scripts/views/pages/home.js
@@ -4,14 +4,11 @@ import { createMovieItemTemplate } from '../templates/template-creator';
 const Home = {
   async render() {
     return `
-    <picture>
-<<<<<<< HEAD
-        <source media="(max-width: 800px)" srcset="./images/hero-image_2-small.jpg" type="image/jpeg">
-=======
-        <source media="(max-width: 800px)" srcset="./images/hero-image_2-small.jpg type="image/jpeg">
->>>>>>> 6298a30ae0786be9368446d12ecc03406dcb4b49
-        <img src="./images/hero-image_2-large.jpg" alt="pembuka web">
-      </picture>
+      <picture>
+                <source media="(max-width: 800px)" srcset="./images/hero-image_2-small.jpg" type="image/jpeg">
+                <source media="(max-width: 800px)" srcset="./images/hero-image_2-small.jpg" type="image/jpeg">
+                <img src="./images/hero-image_2-large.jpg" alt="pembuka web">
+              </picture>
       <div class="content">
         <h2 class="content__heading">Eating House</h2>
         <div id="movies" class="movies">
